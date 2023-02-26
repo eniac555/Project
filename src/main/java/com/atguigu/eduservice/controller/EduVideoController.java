1424f506d563158831a554213182f6641c26c9b6
eniac555 <1150304664@qq.com>
2023-02-26 08:17:08 +0800

    first commit

--- src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,11 +1,13 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.service.EduVideoService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,11 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2023-02-23
  */
-@Api(description = "视频")
+@Api(description = "添加小节")
 @RestController
 @CrossOrigin
 @RequestMapping("/eduservice/edu-video")
 public class EduVideoController {
+    @Autowired
+    private EduVideoService videoService;
+
+    //1.添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+    //2.删除小节
+    //TODO 后面这个需要完善，删除小节的时候，同时把里面的视频删除
+    @DeleteMapping("{id}")
+    public R deleteVideo(@PathVariable String id){
+        videoService.removeById(id);
+        return R.ok();
+    }
+
+    //3.修改小节 todo
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        videoService.updateById(eduVideo);
+        return R.ok();
+    }
+
 
 }
 
