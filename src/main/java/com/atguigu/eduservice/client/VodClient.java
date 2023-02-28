3a59fe461fbbdc4ffa9a74cc250d6e2f506209dc
eniac555 <1150304664@qq.com>
2023-02-28 16:34:07 +0800

    first commit

--- src/main/java/com/atguigu/eduservice/client/VodClient.java
+++ src/main/java/com/atguigu/eduservice/client/VodClient.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Component//交给spring管理
 @FeignClient("service-vod")//被调用的服务的名字，具体名字在application.properties文件里
@@ -15,6 +18,12 @@ public interface VodClient {
     //根据视频id删除阿里云中的视频
     @DeleteMapping("/eduvod/video/removeAlyVideo/{id}")//路径写全
     public R removeAlyVideo(@PathVariable("id") String id);
+    //@PathVariable注解一定要指定参数名称，否则出错
+
+
+    //定义删除多个视频的方法
+    @DeleteMapping("/eduvod/video/delete-batch")//路径写全
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList);
     //@PathVariable注解一定要指定参数名称，否则出错
 
 }
